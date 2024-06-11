8fc300eec4ee493b6c6c661f90871eed911906bd
Samue2408 <samuelmaldonadom@gmail.com>
2024-06-11 16:45:54 -0500

    mi primer commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
+import { montserrat } from './ui/fonts';
 
 export default function Page() {
   return (
@@ -10,8 +12,8 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <p className={`${montserrat.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Bienvenido al curso.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -25,7 +27,21 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* <img src="/hero-desktop.png" alt="Screenshots of the dashboard" /> //no es recomendable la etiqueta img */}
+          <Image
+            src='/hero-desktop.png'
+            alt='Screenshots of the dashboard'
+            width={1000}
+            height={760}
+            className='hidden md:block'
+          />
+          <Image
+            src='/hero-mobile.png'
+            alt='Screenshots of the dashboard'
+            width={560}
+            height={620}
+            className='block md:hidden'
+          />
         </div>
       </div>
     </main>
